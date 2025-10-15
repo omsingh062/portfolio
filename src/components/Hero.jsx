6d257cbc3e5a4cb30d13915718685db5358a4f31
omsingh06 <omsinghhw22@gmail.com>
2025-10-15 22:33:41 +0530

    first

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section id="hero">
       <img src={profilePic} alt="Profile" className="profile-pic" />
-      <h1>Hello, I'm Om Singh Rathod</h1>
+      <h1>Hello, I'm Om Singh Rathour</h1>
       <p>Frontend Developer | JavaScript Enthusiast | Web Designer</p>
       <a href="/resume.pdf" target="_blank" className="btn">
         Download Resume
