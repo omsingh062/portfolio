044706ba3e6dff43dde92dee7698d0cf1c5148ed
omsingh06 <omsinghhw22@gmail.com>
2025-10-29 20:05:22 +0530

    Fix: remove extra spaces in project3 image path

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import project1 from "../images/project1.png";
 import project2 from "../images/project2.png";
-import project3 from "../images/project3.png  ";
+import project3 from "../images/project3.png";
 
 const Projects = () => {
   const projectList = [
