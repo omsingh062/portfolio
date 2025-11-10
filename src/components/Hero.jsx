b176a8ef78aad47c38e26c7d4382d225b6b1a844
omsingh <omsinghhw22@gmail.com>
2025-11-10 13:52:26 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <img src={profilePic} alt="Profile" className="profile-pic" />
       <h1>Hello, I'm Om Singh Rathour</h1>
       <p>Frontend Developer | JavaScript Enthusiast | Web Designer</p>
-      <a href="/Om_Singh_resume.pdf" target="_blank" className="btn">
+      <a href="/Om_Singh Resume.pdf" target="_blank" className="btn">
         Download Resume
       </a>
     </section>
